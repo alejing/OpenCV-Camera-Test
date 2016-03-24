087d5395a486e53187578035ff6b551f690e8bc8
Alejandro Franco <alejing@gmail.com>
2016-03-24 13:56:18 -0500

    Cámara simple
    Muestra video desde la cámara de elección.

--- OpenCV-Camera-Test/main.cpp
+++ OpenCV-Camera-Test/main.cpp
@@ -6,10 +6,28 @@
 //  Copyright © 2016 Alejandro Franco. All rights reserved.
 //
 
+#include <opencv2/highgui/highgui.hpp>
 #include <iostream>
+using namespace std;
+using namespace cv;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main(void)
+{
+    Mat frame;
+    //open camera no 0 internal (0 internal or 1 external)
+    VideoCapture cap(0);
+    //check if internal camera is working
+    while (!(cap.grab() && cap.isOpened()))
+    {
+        cout << "La cámara no esta lista" << endl;
+    }
+    //wait 30 millisecond and check for "s" 115 key number to exit
+    while (waitKey(30) != 115)
+    {
+        //cap >> frame;
+        // read image from camera and store it in frame variable
+        cap.read(frame);
+        // show frame to window named “Cámara”
+        imshow("Cámara",frame);
+    }
 }
